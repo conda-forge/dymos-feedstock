ce820bc1c7f5ce6abdb09aadebc4f3fa7767c31a
swryan <swryan@users.noreply.github.com>
2025-01-13 09:52:57 -0500

    test with numpy<2; re-enable some tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -16,12 +16,12 @@ test_files_to_delete = [
     ["visualization", "linkage", "test", "test_gui.py"],
     ["visualization", "linkage", "test", "linkage_report_ui_test.py"],
     # unsure why this test is failing, but skipping it for now
-    ["examples", "finite_burn_orbit_raise", "test", "test_ex_two_burn_orbit_raise.py"],
+    #["examples", "finite_burn_orbit_raise", "test", "test_ex_two_burn_orbit_raise.py"],
     # skipping because reports/problem203/traj_results_report.html is not being created
-    ["examples", "finite_burn_orbit_raise", "test", "test_ex_two_burn_orbit_raise_bokeh_plots.py"],
+    #["examples", "finite_burn_orbit_raise", "test", "test_ex_two_burn_orbit_raise_bokeh_plots.py"],
     # fails on windows:
-    ["visualization", "test", "test_timeseries_plots.py"],
-    ["test", "test_lint_docstrings.py"],
+    #["visualization", "test", "test_timeseries_plots.py"],
+    #["test", "test_lint_docstrings.py"],
 ]
 
 [
